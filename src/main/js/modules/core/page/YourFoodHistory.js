2042352a154c7882f243583fecdf1172be3cc71f
kubehe <jakub.k.b@hotmail.com>
2019-01-20 17:08:24 +0100

    user can add records to food history

--- src/main/js/modules/core/page/YourFoodHistory.js
+++ src/main/js/modules/core/page/YourFoodHistory.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import moment from "moment";
+import {Button, Card, CardBody, CardHeader, Table} from "reactstrap";
+import {getCurrentUserName, getFoods, getUser, removeFoodHistory} from "../../../api/api";
+import FoodHistoryModal from "./modal/FoodHistoryModal";
+
+const mapUserData = (userData, fetchData, userName) => (userData.map(({food, calories, dateOfConsumption}) =>
+  <tr key={dateOfConsumption}>
+    <td>{food}</td>
+    <td>{calories} cal</td>
+    <td>{moment.unix(dateOfConsumption).calendar()}</td>
+    <td><Button onClick={() => removeFoodHistory({food, name: userName, dateOfConsumption}).then(() => fetchData())}
+                color="danger">Delete</Button></td>
+  </tr>
+));
 
 const YourFoodHistory = () => {
+  const [userData, setUserData] = useState([]);
+  const [userName, setUserName] = useState("");
+
+  const fetchData = () => getCurrentUserName().then((res) => {
+    setUserName(res.data.user);
+    getUser(res.data.user).then((response) => setUserData(response.data.userFoodHistory === null ? [] : response.data.userFoodHistory))
+  });
+
+  const [foodList, setFoodList] = useState([]);
+  useEffect(() => {
+    getFoods().then((res) => setFoodList(res.data));
+  }, []);
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>your food</div>
+    <Card className="my-4">
+      <CardHeader>Foods</CardHeader>
+      <CardBody>
+        <Table>
+          <thead>
+          <tr>
+            <th>Food Name</th>
+            <th>Calories</th>
+            <th>Date of consumption</th>
+            <th>Action</th>
+          </tr>
+          </thead>
+          <tbody>
+          {mapUserData(userData, fetchData, userName)}
+          </tbody>
+        </Table>
+        <FoodHistoryModal foodList={foodList} onOk={fetchData} name={userName}/>
+      </CardBody>
+    </Card>
   )
 };
 
---
+++ src/main/js/modules/core/page/modal/FoodHistoryModal.js
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from "react";
+import {
+  Button,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader
+} from "reactstrap";
+import {addFoodHistory, getFoods} from "../../../../api/api";
+import moment from "moment";
+
+const ModalComponent = ({onOk, name, foodList}) => {
+  const [food, setFood] = useState("");
+  const [time, setTime] = useState(null);
+  const [date, setDate] = useState(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen((prev) => !prev);
+
+  return (
+    <>
+      <Button color="success" onClick={() => setIsOpen(true)}>Add</Button>
+      <Modal isOpen={isOpen} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add Record of eaten food</ModalHeader>
+        <Form>
+          <ModalBody>
+            <FormGroup>
+              <Label for="SelectMulti">Food</Label>
+              <Input onChange={(e) => setFood(e.target.value)} type="select" name="selectMulti" id="SelectMulti"
+                     multiple invalid={food == null || !foodList.map((food) => food.name).includes(food)}
+                     valid={foodList.map((food) => food.name).includes(food)}>
+                {foodList.map((food) => food.name).map((name) => <option key={name}>{name}</option>)}
+              </Input>
+              <FormFeedback valid>You have chosen food successfully</FormFeedback>
+              <FormFeedback invalid>Oh no, please choose food :/</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+              <Label for="date">Date</Label>
+              <Input onChange={(e) => setDate(e.target.value)} type="date" name="date" id="date"
+                     invalid={date == null} valid={date != null}>
+              </Input>
+              <FormFeedback valid>You have chosen date successfully</FormFeedback>
+              <FormFeedback invalid>Oh no, please choose date :/</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+              <Label for="time">Time</Label>
+              <Input onChange={(e) => setTime(e.target.value)} type="time" name="time" id="time"
+                     invalid={time == null} valid={time != null}>
+              </Input>
+              <FormFeedback valid>You have chosen time successfully</FormFeedback>
+              <FormFeedback invalid>Oh no, please choose time :/</FormFeedback>
+            </FormGroup>
+
+          </ModalBody>
+          <ModalFooter>
+            <Button disabled={food == null || !foodList.map((food) => food.name).includes(food)}
+                    color="primary" type="submit" onClick={(e) => {
+              e.preventDefault();
+              toggle();
+              addFoodHistory({dateOfConsumption: moment(date, 'YYYY-MM-DD').unix(), name, food}).then(() => onOk())
+            }}>Add</Button>
+            <Button color="secondary" type="reset" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
+        </Form>
+      </Modal>
+    </>
+  )
+
+};
+
+export default ModalComponent;
